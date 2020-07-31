9e05db002d0a5c5584654548c3477a5e3afc284b
Christofer Gabriel Pinheiro <48489750+Retr078@users.noreply.github.com>
2020-07-31 20:12:25 +0800

    just staging

--- Question 6/question6.cpp
+++ Question 6/question6.cpp
@@ -1,172 +1,172 @@
 //our c++ implementation of 8 puzzle
-#include <bits/stdc++.h> 
-using namespace std; 
-#define N 3 
+#include <bits/stdc++.h>
+using namespace std;
+#define N 3
 
-// state space tree nodes 
-struct Node 
-{ 
+// state space tree nodes
+struct Node
+{
 
-	Node* parent; 
-
- 
-	int mat[N][N]; 
+	Node* parent;
 
 
-	int x, y; 
-
-	int cost; 
-
- 
-	int level; 
-}; 
-
-// Function to print N x N matrix 
-int printMatrix(int mat[N][N]) 
-{ 
-	for (int i = 0; i < N; i++) 
-	{ 
-		for (int j = 0; j < N; j++) 
-			printf("%d ", mat[i][j]); 
-		printf("\n"); 
-	} 
-} 
+	int mat[N][N];
 
 
-Node* newNode(int mat[N][N], int x, int y, int newX, 
-			int newY, int level, Node* parent) 
-{ 
-	Node* node = new Node; 
+	int x, y;
+
+	int cost;
 
 
-	node->parent = parent; 
+	int level;
+};
 
-	memcpy(node->mat, mat, sizeof node->mat); 
-
-
-	swap(node->mat[x][y], node->mat[newX][newY]); 
-
-	node->cost = INT_MAX; 
-
-	node->level = level; 
-
-
-	node->x = newX; 
-	node->y = newY; 
-
-	return node; 
-} 
-
-// botton, left, top, right 
-int row[] = { 1, 0, -1, 0 }; 
-int col[] = { 0, -1, 0, 1 }; 
+// Function to print N x N matrix
+int printMatrix(int mat[N][N])
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+			printf("%d ", mat[i][j]);
+		printf("\n");
+	}
+}
 
 
-int calculateCost(int initial[N][N], int final[N][N]) 
-{ 
-	int count = 0; 
-	for (int i = 0; i < N; i++) 
-	for (int j = 0; j < N; j++) 
-		if (initial[i][j] && initial[i][j] != final[i][j]) 
-		count++; 
-	return count; 
-} 
+Node* newNode(int mat[N][N], int x, int y, int newX,
+			int newY, int level, Node* parent)
+{
+	Node* node = new Node;
 
 
-int isSafe(int x, int y) 
-{ 
-	return (x >= 0 && x < N && y >= 0 && y < N); 
-} 
+	node->parent = parent;
 
-void printPath(Node* root) 
-{ 
-	if (root == NULL) 
-		return; 
-	printPath(root->parent); 
-	printMatrix(root->mat); 
+	memcpy(node->mat, mat, sizeof node->mat);
 
-	printf("\n"); 
-} 
 
-// Comparison object to be used to order the heap 
-struct comp 
-{ 
+	swap(node->mat[x][y], node->mat[newX][newY]);
+
+	node->cost = INT_MAX;
+
+	node->level = level;
+
+
+	node->x = newX;
+	node->y = newY;
+
+	return node;
+}
+
+// botton, left, top, right
+int row[] = { 1, 0, -1, 0 };
+int col[] = { 0, -1, 0, 1 };
+
+
+int calculateCost(int initial[N][N], int final[N][N])
+{
+	int count = 0;
+	for (int i = 0; i < N; i++)
+	for (int j = 0; j < N; j++)
+		if (initial[i][j] && initial[i][j] != final[i][j])
+		count++;
+	return count;
+}
+
+
+int isSafe(int x, int y)
+{
+	return (x >= 0 && x < N && y >= 0 && y < N);
+}
+
+void printPath(Node* root)
+{
+	if (root == NULL)
+		return;
+	printPath(root->parent);
+	printMatrix(root->mat);
+
+	printf("\n");
+}
+
+// Comparison object to be used to order the heap
+struct comp
+{
 	bool operator()(const Node* lhs, const Node* rhs) const
-	{ 
-		return (lhs->cost + lhs->level) > (rhs->cost + rhs->level); 
-	} 
-}; 
+	{
+		return (lhs->cost + lhs->level) > (rhs->cost + rhs->level);
+	}
+};
 
-void solve(int initial[N][N], int x, int y, 
-		int final[N][N]) 
-{ 
+void solve(int initial[N][N], int x, int y,
+		int final[N][N])
+{
 
-	priority_queue<Node*, std::vector<Node*>, comp> pq; 
+	priority_queue<Node*, std::vector<Node*>, comp> pq;
 
-	// create a root node and calculate its cost 
-	Node* root = newNode(initial, x, y, x, y, 0, NULL); 
-	root->cost = calculateCost(initial, final); 
+	// create a root node and calculate its cost
+	Node* root = newNode(initial, x, y, x, y, 0, NULL);
+	root->cost = calculateCost(initial, final);
 
-	// Add root to list of live nodes; 
-	pq.push(root); 
+	// Add root to list of live nodes;
+	pq.push(root);
 
-	while (!pq.empty()) 
-	{ 
-		// Find a live node with least estimated cost 
-		Node* min = pq.top(); 
-
-	
-		pq.pop(); 
-
-	
-		if (min->cost == 0) 
-		{ 
-	
-			printPath(min); 
-			return; 
-		} 
-
-	
-		for (int i = 0; i < 4; i++) 
-		{ 
-			if (isSafe(min->x + row[i], min->y + col[i])) 
-			{ 
-
-				Node* child = newNode(min->mat, min->x, 
-							min->y, min->x + row[i], 
-							min->y + col[i], 
-							min->level + 1, min); 
-				child->cost = calculateCost(child->mat, final); 
-
-				// Add child to list of live nodes 
-				pq.push(child); 
-			} 
-		} 
-	} 
-} 
+	while (!pq.empty())
+	{
+		// Find a live node with least estimated cost
+		Node* min = pq.top();
 
 
-int main() 
-{ 
-	int initial[N][N] = 
-	{ 
-		{1, 2, 3}, 
-		{5, 6, 0}, 
-		{7, 8, 4} 
-	}; 
+		pq.pop();
 
 
-	int final[N][N] = 
-	{ 
-		{1, 2, 3}, 
-		{5, 8, 6}, 
-		{0, 7, 4} 
-	}; 
+		if (min->cost == 0)
+		{
+
+			printPath(min);
+			return;
+		}
 
 
-	int x = 1, y = 2; 
+		for (int i = 0; i < 4; i++)
+		{
+			if (isSafe(min->x + row[i], min->y + col[i]))
+			{
 
-	solve(initial, x, y, final); 
+				Node* child = newNode(min->mat, min->x,
+							min->y, min->x + row[i],
+							min->y + col[i],
+							min->level + 1, min);
+				child->cost = calculateCost(child->mat, final);
 
-	return 0; 
-} 
+				// Add child to list of live nodes
+				pq.push(child);
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	int initial[N][N] =
+	{
+		{1, 2, 3},
+		{5, 6, 0},
+		{7, 8, 4}
+	};
+
+
+	int final[N][N] =
+	{
+		{1, 2, 3},
+		{5, 8, 6},
+		{0, 7, 4}
+	};
+
+
+	int x = 1, y = 2;
+
+	solve(initial, x, y, final);
+
+	return 0;
+}
